503ccd3c74669cc1946e188f7e933f8e490cc547
oOBlinkOo <rivensona@gmail.com>
2017-01-20 14:55:21 -0600

    full test of this pass of  parameters angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,27 @@ import {VoteTakerComponent} from './voteTakerComponent';
 import {CountdownTimerComponent} from './countDownTimerComponent';
 import {CountdownLocalVarParentComponent} from './countdownocal';
 import {CountdownViewChildParentComponent} from  './countviewChildParentComponent';
+import {MissionService} from './mission.service';
+import {MissionControlComponent} from './misionComponent';
+import {AstronautComponent} from './astronaut.componet';
+
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,VoterComponent,VoteTakerComponent,CountdownTimerComponent,CountdownLocalVarParentComponent,CountdownViewChildParentComponent
+    AppComponent,VoterComponent,VoteTakerComponent,CountdownTimerComponent,
+    CountdownLocalVarParentComponent,CountdownViewChildParentComponent,
+    AstronautComponent,MissionControlComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [MissionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
